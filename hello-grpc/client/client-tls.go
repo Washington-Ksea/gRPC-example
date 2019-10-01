26d054dd2776dabd75e4478ba102f36691ee6870
Washington-Ksea <ksea.washington@gmail.com>
2019-10-01 11:57:22 +0900

    Add ssl certification mode

---
+++ hello-grpc/client/client-tls.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"google.golang.org/genproto/googleapis/rpc/errdetails"
+
+	pb "github.com/Washington-Ksea/gRPC-example/hello-grpc"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/status"
+)
+
+func main() {
+	addr := "localhost:50051"
+	cred, err := credentials.NewClientTLSFromFile("server.crt", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+	conn, err := grpc.Dial(addr, grpc.WithTransportCredentials(cred))
+	if err != nil {
+		log.Fatalf("did not conect %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := os.Args[1]
+
+	ctx := context.Background()
+	//ctx, cancel := context.WithCancel(ctx)
+	md := metadata.Pairs("timestamp", time.Now().Format(time.Stamp))
+	ctx = metadata.NewOutgoingContext(ctx, md)
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name}, grpc.Trailer(&md))
+	if err != nil {
+		s, ok := status.FromError(err)
+		if ok {
+			log.Printf("gRPC Error (message %s)", s.Message())
+			for _, d := range s.Details() {
+				switch info := d.(type) {
+				case *errdetails.RetryInfo:
+					log.Printf(" RetryInfo: %v", info)
+				}
+			}
+			os.Exit(1)
+		} else {
+			log.Fatalf("could not greet %v", err)
+		}
+
+	}
+	log.Printf("Greeting: %s", r.Message)
+
+}
--- hello-grpc/cmd.md
+++ hello-grpc/cmd.md
@@ -8,3 +8,15 @@ protoc greeter.proto --go_out=plugins=grpc:.
 //http2のデバッグモード
 export GODEBUG=http2debug=2
 ```
+
+ssl 
+
+```
+brew install openssl
+
+openssl version
+
+openssl genrsa 2048 > private.key
+openssl req -new -key private.key > server.csr
+openssl x509 -days 367 -req -signkey private.key < server.csr > server.crt
+```
